e355f73ce4ac54aa17b3ea84a6673428ff687221
Fernando <feh.wilinando@gmail.com>
2019-09-26 21:19:20 -0300

    Setup project

---
+++ distancia/src/main/java/me/fwfurtado/DistanciaApplication.java
@@ -0,0 +1,12 @@
+package me.fwfurtado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistanciaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistanciaApplication.class, args);
+    }
+}
